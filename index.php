e5ca22d391f536f39161bb099e69e5434f5ec46a
Hugo <huhu051@gmail.com>
2020-10-20 00:25:08 +0200

    Revert "rajout d'un caca"
    This reverts commit 27a9d512cab3894a6c6ea0e14218bb7629bf2ba2.

--- index.php
+++ index.php
@@ -17,7 +17,6 @@ echo '<div class="mx-auto" style="width: 1200px;">';
 $idM = 0;
 affichematch($idM);
 echo '</div>';
-echo "caca";
 ?>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
         integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
